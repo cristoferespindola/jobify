b1ed1d0a25a8bc86cee1793fb984b08bc1dc1c4c
Cristofer <cristoferespindola@outlook.com>
2019-11-27 19:38:36 -0300

    Preparando deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dbConnection = sqlite.open('banco.sqlite', { Promise });
 
 const bodyParser =  require('body-parser');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
