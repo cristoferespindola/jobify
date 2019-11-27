61e6877204edab7e5cebfb9b265e6d1e4fd0c9c1
Cristofer <cristoferespindola@outlook.com>
2019-11-27 20:41:19 -0300

    Ajuste path views

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const bodyParser =  require('body-parser');
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views/'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
